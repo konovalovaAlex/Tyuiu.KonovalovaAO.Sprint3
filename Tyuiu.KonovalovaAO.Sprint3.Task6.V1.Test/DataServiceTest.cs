38abccc7e01038988aadb76517ce33847b0a864f
konovalovaAlex <144355131+konovalovaAlex@users.noreply.github.com>
2023-10-29 17:36:48 +0500

    Task6

---
+++ Tyuiu.KonovalovaAO.Sprint3.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KonovalovaAO.Sprint3.Task6.V1.Lib;
+namespace Tyuiu.KonovalovaAO.Sprint3.Task6.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 11;
+            int stopValue = 17;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 151;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KonovalovaAO.Sprint3.Task6.V1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KonovalovaAO.Sprint3.Task6.V1.Lib;  
+namespace Tyuiu.KonovalovaAO.Sprint3.Task6.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнила Коновалова А. О. /СМАРТб-23-1 ";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                               *");
+            Console.WriteLine("* Задание #6                                                             *");
+            Console.WriteLine("* Вариант #1                                                             *");
+            Console.WriteLine("* выполнила: Коновалова Александра Олеговна | СМАРТб-23-1                *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих      *");
+            Console.WriteLine("* числовому отрезку [11, 17] сумму всех делителей.                       *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int startValue = 11;
+            int stopValue = 17;
+            Console.WriteLine("Начало отрезка = " + startValue);
+            Console.WriteLine("Конец отрезка = " + stopValue);
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("Сумма делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
